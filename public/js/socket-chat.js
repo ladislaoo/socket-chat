85c63b4103b82246218b8ca14e5bd11751d715cd
Ladislao Osorio <losorio@sunset.com.mx>
2020-03-10 15:49:56 -0500

    chat finalizado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,18 +1,27 @@
 var socket = io();
-var params= new URLSearchParams (window.location.search);
-if(!params.has('nombre')||!params.has('sala')){
-    window.location='index.html';
+
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
-var usuario ={
+
+var usuario = {
     nombre: params.get('nombre'),
-    sala:params.get('sala')
-}
+    sala: params.get('sala')
+};
+
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
-    socket.emit('entrarChat',usuario,function(resp){
-        console.log('Usuarios conectados',resp)
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -24,8 +33,8 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-// socket.emit('enviarMensaje', {
-//     usuario: 'Fernando',
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -33,16 +42,20 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje,false);
+    scrollBottom();
 });
-//Escuchar cambios de usuarios
+
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+    renderizarUsuarios(personas);
 });
-socket.on('mensajePrivado',function(mensaje){
-    console.log('mensaje Privado',mensaje);
+
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
